dd0644b728e7c0c82f97bef24162466fe422cf9a
Vito9009 <yan.vi193@gmail.com>
2021-12-19 15:47:58 +0100

    palindromo-con-funzione

--- Palindromo/myscript.js
+++ Palindromo/myscript.js
@@ -11,26 +11,32 @@ let lowercaseAskWord = askWord.toLowerCase(); // Rendere la parola minuscola (pe
 let wordArray = []; // Array vuoto per la parola inserita
 let reverseWordArray = []; // Array vuoto per la parola inserita partendo dall'ultima lettera
 
-for(i = 0; i < askWord.length; i++){ // push della parola inserita nella apposita array
-    wordArray.push(askWord.toLowerCase()[i]);
-}
-console.log(wordArray);
+function palindrome(){
+    for(i = 0; i < askWord.length; i++){ // push della parola inserita nella apposita array
+        wordArray.push(askWord.toLowerCase()[i]);
 
-for(i = askWord.length - 1; i >= 0; i--){ // push della parola inserita al rovescio nella apposita array
-    reverseWordArray.push(askWord.toLowerCase()[i]);
-}
-console.log(reverseWordArray);
+        for(i = askWord.length - 1; i >= 0; i--){ // push della parola inserita al rovescio nella apposita array
+            reverseWordArray.push(askWord.toLowerCase()[i]);
 
-if(JSON.stringify(wordArray) == JSON.stringify(reverseWordArray)){ // Se la parola inserita è uguale alla stessa parola scritta nel senso inverso, allora sarà un palindromo
-    console.log("La parola che hai scelto è un palindromo");
-}else{ // Se la parola inserita non è uguale alla stessa parola scritta nel senso inverso, allora non sarà un palindromo
-    console.log("La parola che hai scelto non è un palindromo");
+            if(JSON.stringify(wordArray) == JSON.stringify(reverseWordArray)){ // Se la parola inserita è uguale alla stessa parola scritta nel senso inverso, allora sarà un palindromo
+                return "Si, è un palindromo.";
+            }else{ // Se la parola inserita non è uguale alla stessa parola scritta nel senso inverso, allora non sarà un palindromo
+                return "No, non è un palindromo.";
+            }    
+        }        
+    }
 }
 
+let palindromeWord = palindrome();
+
+console.log(askWord + " " + "è un palindromo" + "?");
+console.log(palindromeWord);
 /*
 JSON stringify serve a convertire un oggetto o un valore JavaScript in una stringa JSON.
 In questo caso senza il metodo JSON stringify le parole inserite non erano mai dei palindromi (anche se la parola inserita in realtà lo era).
+
 Concetto non molto chiaro e da rivedere.
 
-Provare a ricreare esercizio utilizzando una funzione!!!
+document.writeln(askWord + " " + "è un palindromo" + "?" + "<br>");
+document.writeln(palindromeWord);
 */
